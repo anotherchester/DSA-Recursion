a9334522408b54115a63a0abbe54cb2fd6137d8d
anotherchester <anotherchester@gmail.com>
2020-04-16 18:18:10 -0700

    two more solved

--- drills-day1.js
+++ drills-day1.js
@@ -37,15 +37,18 @@ console.log(triangle(5))
 
 //string splitter
 
-function stringSplitter(str, separator) {
- if(!str.includes(separator)) {return [str]}
-
- else {
-  const sepIdx = str.indexOf(separator) 
-  const sliced = str.slice(0,sepIdx)
+function strSplitter(str, sep, cur = "") {
+  console.log(str);
+  if (str.length === 0) {
+    return [cur];
   }
+  if (str[0] == "/") {
+    return [cur, ...strSplitter(str.substr(1), sep, "")]
+  }
+  return strSplitter(str.substr(1), sep, cur + str[0])
 }
-console.log(stringSplitter('02/20/2020'))
+result = strSplitter("20/20/2020", "/")
+console.log(result);
 
 //fibonacci
 
@@ -65,3 +68,37 @@ function factorial(x) {
 }
 console.log(factorial(5))
 
+//maze
+
+let mySmallMaze = [
+  [' ', ' ', ' '],
+  [' ', '*', ' '],
+  [' ', ' ', 'e']
+];
+let maze = [
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  ['*', '*', ' ', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', '*', '*', '*', '*', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+function solveMave(myMaze, row, column) {
+  if (myMaze.length < row || myMaze.length < column || row < 0 || column < 0) {
+    return;
+  }
+  const value = myMaze[row][column]
+  if (value == "e") {
+    console.log("exit found");
+    return;
+  }
+  if (value == "*") {
+    return;
+  }
+  myMaze[row][column] = "*"
+  solveMave(myMaze, row + 1, column)
+  solveMave(myMaze, row, column + 1)
+  solveMave(myMaze, row - 1, column)
+  solveMave(myMaze, row, column - 1)
+  myMaze[row][column] = " "
+}
+solveMave(myMaze, 0, 0)
